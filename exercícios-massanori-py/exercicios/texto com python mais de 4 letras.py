630a0f67a71abfc60d0314e79508ab488f9fb453
Mandella-thi <thiagogarc@gmail.com>
2021-02-10 22:51:26 -0300

    texto com caracteres de python com mais de 4 letras

---
+++ exercícios-massanori-py/exercicios/texto com python mais de 4 letras.py
@@ -0,0 +1,16 @@
+var='The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'
+lista=[]
+var=var.replace(',',' ')
+var=var.replace('.', ' ')
+var=var.lower()
+var=var.split(' ')
+x=0
+print(var)
+for k in var:
+    if k[0::] in 'python' or k[-1] in 'python':
+        lista.append(k)
+print(lista)
+for m in lista:
+    if len(m)>=4:
+        x=x+1
+print(x)
