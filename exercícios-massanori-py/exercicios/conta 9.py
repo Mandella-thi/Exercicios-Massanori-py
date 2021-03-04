de8e3f2d577574ccae61bb2fa2470492bbaa452c
Mandella-thi <thiagogarc@gmail.com>
2021-03-04 11:59:43 -0300

    join e multistring

---
+++ exercícios-massanori-py/exercicios/conta 9.py
@@ -0,0 +1,6 @@
+# C. array_count9
+# conta quantas vezes aparece o 9 numa lista nums
+def array_count9(nums):
+  return nums.count(9)
+
+print(array_count9([1,2,99,7,9,9,]))
---
+++ exercícios-massanori-py/exercicios/lista 7 hello fulano.py
@@ -0,0 +1,9 @@
+# E. hello_name
+# seja uma string name
+# hello_name('Bob') -> 'Hello Bob!'
+# hello_name('Alice') -> 'Hello Alice!'
+# hello_name('X') -> 'Hello X!'
+def hello_name(name):
+  return f"hello {name}"
+
+print(hello_name('Thiago'))
