e9ec051a872362347258bb7373cf397e47e00c4b
Mandella-thi <thiagogarc@gmail.com>
2021-03-04 13:29:27 -0300

    first and last 6

---
+++ exercícios-massanori-py/exercicios/lista 9 first last 6.py
@@ -0,0 +1,9 @@
+# A. first_last6
+# verifica se 6 é o primeiro ou último elemento da lista nums
+# first_last6([1, 2, 6]) -> True
+# first_last6([6, 1, 2, 3]) -> True
+# first_last6([3, 2, 1]) -> False
+def first_last6(nums): #
+  return  nums[0]==6 or  nums[-1]==6
+
+print(first_last6([3,6,4]))
