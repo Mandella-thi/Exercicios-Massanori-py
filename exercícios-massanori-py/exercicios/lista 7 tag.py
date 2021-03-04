f378782723b44c71a890e7faed5646356f7236bf
Mandella-thi <thiagogarc@gmail.com>
2021-03-04 12:20:32 -0300

    make tags

---
+++ exercícios-massanori-py/exercicios/lista 7 tag.py
@@ -0,0 +1,10 @@
+#F. make_tags
+# make_tags('i', 'Yay'), '<i>Yay</i>'
+# make_tags('i', 'Hello'), '<i>Hello</i>'
+# make_tags('cite', 'Yay'), '<cite>Yay</cite>'
+def make_tags(tab, word):
+  return f"<{tab}> {word} </{tab}>"
+
+print(make_tags('i', "tom"))
+
+
