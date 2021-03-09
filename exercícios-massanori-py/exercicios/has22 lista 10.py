19736fed7575f131d9bf03eff8e8cce6f8451de1
Mandella-thi <thiagogarc@gmail.com>
2021-03-09 09:28:26 -0300

    end, count code count evens

---
+++ exercícios-massanori-py/exercicios/has22 lista 10.py
@@ -0,0 +1,8 @@
+# K. has22 #
+# Verifica se na lista de números inteiros aparecem dois 2 consecutivos
+# has22([1, 2, 2]) -> True
+# has22([1, 2, 1, 2]) -> False
+# has22([2, 1, 2]) -> False
+def has22(nums):
+    return  '2, 2' in str(nums)
+print(has22([2,1, 2]))
---
+++ exercícios-massanori-py/exercicios/soma lista lista 10.py
@@ -0,0 +1,12 @@
+# L. soma_na_lista #
+# Verifica se um número é soma de dois elementos distintos de uma lista
+# soma_na_lista(5, [1, 2, 3, 4]) -> True
+# soma_na_lista(9, [1, 2, 3, 4]) -> False
+# soma_na_lista(0, [1, 2, 3, 4]) -> False
+# soma_na_lista(8, [1, 2, 3, 4]) -> False
+# soma_na_lista(4, [2, 2, 2, 2]) -> False
+# soma_na_lista(4, [2, 2, 1, 3]) -> True
+def soma_na_lista(n, lista):
+  return n in [x+y for x in lista for y in lista if x!=y]
+
+print(soma_na_lista(5, [1,2,3]))
