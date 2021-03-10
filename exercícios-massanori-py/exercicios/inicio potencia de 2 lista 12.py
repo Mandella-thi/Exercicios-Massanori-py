50ffcfa1d5c8443e7d4080d812eb0659c34e8375
Mandella-thi <thiagogarc@gmail.com>
2021-03-10 15:31:02 -0300

    inicio pot 2

--- exercícios-massanori-py/exercicios/inicio potencia de 2 lista 12.py
+++ exercícios-massanori-py/exercicios/inicio potencia de 2 lista 12.py
@@ -3,5 +3,11 @@
 # que tenha o início igual a n
 # Exemplo: para n = 65 retornará 16 pois 2**16 = 65536
 def inip2(n):
+    k=0
+    while True:
+        pot= str(2**k)
+        if pot.startswith((str(n))):
+            return k
+        k=k+1
 
-  return
+print(inip2(7))
