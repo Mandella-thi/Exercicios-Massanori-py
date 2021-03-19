1c944d9916569dc9531ea972a519aee7f8488e63
Mandella-thi <thiagogarc@gmail.com>
2021-03-19 12:16:22 -0300

    ocultador de arquivos

--- exercícios-massanori-py/exercicios/ocultador de arquivos.py
+++ exercícios-massanori-py/exercicios/ocultador de arquivos.py
@@ -1,2 +1,10 @@
 import ctypes
 
+atributo_ocultar=0x02
+
+retorno= ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
+
+if retorno:
+    print("arquivo ocultado")
+else:
+    print('arquivo não foi ocultado')
